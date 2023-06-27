c42e50f91526507f5befddda6025666e0fd0e5c3
Yevhenii Filatov <yevhenii.filatov@extrawest.com>
2023-06-27 19:13:17 +0300

    updated comment service implementation

--- src/main/java/com/it/service/impl/CommentServiceImpl.java
+++ src/main/java/com/it/service/impl/CommentServiceImpl.java
@@ -5,8 +5,10 @@ import com.it.mapper.CommentMapper;
 import com.it.model.domain.Comment;
 import com.it.model.entity.CommentEntity;
 import com.it.model.entity.TaskEntity;
+import com.it.model.entity.UserEntity;
 import com.it.repository.CommentRepository;
 import com.it.repository.TaskRepository;
+import com.it.repository.UserRepository;
 import com.it.service.CommentService;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
     private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
     private final CommentRepository commentRepository;
     private final CommentMapper commentMapper;
 
@@ -49,11 +52,16 @@ public class CommentServiceImpl implements CommentService {
     private Comment doSave(Comment comment) {
         return taskRepository
            .findById(comment.getTaskId())
-           .map(taskEntity -> {
-               CommentEntity mapped = commentMapper.toEntity(comment);
-               mapped.setTask(taskEntity);
-               CommentEntity saved = commentRepository.save(mapped);
-               return commentMapper.fromEntity(saved);
-           }).orElseThrow(() -> NotFoundException.ofId(comment.getTaskId(), TaskEntity.class));
+           .map(taskEntity -> userRepository
+              .findById(comment.getAuthorId())
+              .map(authorUserEntity -> {
+                  CommentEntity mapped = commentMapper.toEntity(comment);
+                  mapped.setTask(taskEntity);
+                  mapped.setAuthor(authorUserEntity);
+                  CommentEntity saved = commentRepository.save(mapped);
+                  return commentMapper.fromEntity(saved);
+              })
+              .orElseThrow(() -> NotFoundException.ofId(comment.getAuthorId(), UserEntity.class))
+           ).orElseThrow(() -> NotFoundException.ofId(comment.getTaskId(), TaskEntity.class));
     }
 }
